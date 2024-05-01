3b907e0854870bc0ed24b5e1ae169f06f074ba6d
Oliver Hughes <oliver.hughes@adevinta.com>
2024-05-01 03:53:34 +0200

    configure autopairs

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -13,5 +13,14 @@ return { -- Add closing brackets, etc
         java = false, -- don't check treesitter on java
       },
     }
+
+    -- import nvim-autopairs completion funcitonality
+    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+
+    -- import nvim-cmp plugin
+    local cmp = require 'cmp'
+
+    -- make autopairs and completion work together
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
