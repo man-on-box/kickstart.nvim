bbdf06f960841940d3c78ade273120ba1ea44b1e
Oliver Hughes <oliver.hughes@adevinta.com>
2024-05-04 22:28:09 +0200

    add tailwind tools

---
+++ lua/custom/plugins/tailwind-tools.lua
@@ -0,0 +1,9 @@
+return {
+  'luckasRanarison/tailwind-tools.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  opts = {
+    document_color = {
+      kind = 'background',
+    },
+  },
+}
